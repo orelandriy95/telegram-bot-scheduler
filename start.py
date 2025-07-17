7df4be34b29aa81e8607df6666a07bc756de1105
orelandriy95 <orelandriy95@gmail.com>
2025-07-17 23:43:38 +0300

    Update88start.py

--- start.py
+++ start.py
@@ -18,11 +18,20 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Авторизація через змінну SERVICE_ACCOUNT_JSON
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-service_account_json = os.getenv("SERVICE_ACCOUNT_JSON")
+import os
+import json
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
 
-# Розпарсимо JSON-рядок
-creds_dict = json.loads(service_account_json)
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+
+# Отримати JSON як рядок з змінної оточення
+creds_json = os.getenv("SERVICE_ACCOUNT_JSON")
+
+# Розпарсити його у словник
+creds_dict = json.loads(creds_json)
+
+# Авторизація
 creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
 
