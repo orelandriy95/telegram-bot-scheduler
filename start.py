089d251cb84d3e1576c58ed335568a47b4ed7560
orelandriy95 <orelandriy95@gmail.com>
2025-07-18 21:04:58 +0300

    iiistart.py

--- start.py
+++ start.py
@@ -3,9 +3,5 @@ import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-# ✅ Завантаження змінної середовища з Railway
-creds_json = os.getenv("SERVICE_ACCOUNT_JSON")
-if not creds_json:
-    raise ValueError("❌ Змінна SERVICE_ACCOUNT_JSON не знайдена")
 
-print("✅ Таблиця знайдена:", creds_json)
+print("✅ Таблиця знайдена:", SERVICE_ACCOUNT_JSON)
