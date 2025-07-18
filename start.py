c2cf8d4d1e680ba3d9fb9816e76f8bc9921463d7
orelandriy95 <orelandriy95@gmail.com>
2025-07-18 21:16:27 +0300

    testtart.py

--- start.py
+++ start.py
@@ -2,6 +2,18 @@ import os
 import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+print('-----------++++--------------')
 
+gc = gspread.service_account("SERVICE_ACCOUNT_JSON")
 
-print("✅ Таблиця знайдена:", "SERVICE_ACCOUNT_JSON")
+# Open a sheet from a spreadsheet in one go
+wks = gc.open("Schedule").sheet1
+
+# Update a range of cells using the top left corner address
+wks.update([[1, 2], [3, 4]], 'A1')
+
+# Or update a single cell
+wks.update_acell('B42', "it's down there somewhere, let me take another look.")
+
+# Format the header
+wks.format('A1:B1', {'textFormat': {'bold': True}})
